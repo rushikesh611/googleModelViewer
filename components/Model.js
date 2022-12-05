2ea6264d576d6f18b29f32f21fa2ce3b5d672cb3
Hrushikesh Jadhav <rushi611@gmail.com>
2022-12-05 13:42:07 +0530

    updated

--- components/Model.js
+++ components/Model.js
@@ -3,7 +3,7 @@ import "@google/model-viewer";
 const Model = () => (
   <div id="card">
     <model-viewer
-      src="/Swastik.glb"
+      src="/Pahlaj.glb"
       ios-src=""
       poster="https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b%2Fposter-astronaut.png?v=1599079951717"
       alt="A 3D model of an astronaut"
